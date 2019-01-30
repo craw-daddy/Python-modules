46ec094cfb43edfa24bc0307d19886af621647f6
Russell Martin <russell@martinandbrown.com>
2019-01-30 14:27:05 -0500

    Some small structural changes, allow for single dice in some functions

--- dice/dice.py
+++ dice/dice.py
@@ -5,6 +5,8 @@ File: dice.py
 Methods for "rolling dice", printing out a list of 
 possible outcomes when dice are rolled, or a probability 
 distribution for a given collection of dice.  
+
+Last updated 30 Jan 2019.  RAM
 """
 __all__ = ['diceDict', 'diceProb', 'roll']
 
@@ -16,7 +18,7 @@ def _mergeDiceDicts(d1, d2):
     A helper method, generally to be used with the "diceDict"
     method defined elsewhere.
     """
-    assert type(d1) == dict and type(d2) == dict, "Invalid argument to mergeDiceDicts!"
+    assert isinstance(d1, dict) and isinstance(d2, dict), "Invalid argument to mergeDiceDicts!"
     if len(d1) == 0:
         return d2
     if len(d2) == 0:
@@ -43,7 +45,7 @@ def diceDict(diceList):
     die (which could be positive, zero, or negative, and could 
     have repetitions).   
     """
-    assert type(diceList) == list, "Invalid argument to diceDict!"
+    assert isinstance(diceList, list), "Invalid argument to diceDict!"
     if len(diceList) == 0:
         return dict()
     elif len(diceList) == 1:
@@ -51,7 +53,7 @@ def diceDict(diceList):
         #  interpret it as a single die where the values of the sides
         #  are the elements of that list.  (Allows for dice with the 
         #  same values on sides, Sicherman dice, negative numbers, etc.)
-        if type(diceList[0]) == list:
+        if isinstance(diceList[0], list):
             newDict = dict()
             for x in set(diceList[0]):
                 newDict[x] = diceList[0].count(x)
@@ -74,7 +76,9 @@ def diceProb(diceList):
     """ 
     Returns a dictionary of probabilities, where the keys are 
     the possible values obtainable with the set of dice in 
-    diceList.  
+    diceList.  diceList is either a single integer (in which
+    case it's recast into a list with one element, see below), 
+    or is a list.
 
     Each element of diceList is a positive number, or another list.  
     If it's a positive number n, it's for a die with that many 
@@ -83,14 +87,19 @@ def diceProb(diceList):
     die (which could be positive, zero, or negative, and could 
     have repetitions).       
     """
-    assert type(diceList) == list, "Invalid argument to diceProb!"
+    assert isinstance(diceList, (list, int)), "Invalid argument to diceProb!"
+    if isinstance(diceList, int):
+        diceList = [ diceList ]    #  recast a single number to a list
     result = diceDict(diceList)
     s = sum(result.values())
     return { x : result[x]/s for x in result.keys() }
 
 def roll(diceList):
     """ 
-    Returns a "roll" of the dice described in diceList.  
+    Returns a "roll" of the dice described in diceList.  diceList
+    is either a single integer (in which case it's assumed to be
+    positive and will be recast into a list of a single item, 
+    see below), or a list.  
     
     Each element of diceList is a positive number, or another list.  
     If it's a positive number n, it's for a die with that many 
@@ -99,13 +108,15 @@ def roll(diceList):
     die (which could be positive, zero, or negative, and could 
     have repetitions).
     """ 
-    assert type(diceList) == list, "Invalid argument to roll!"
+    assert isinstance(diceList, (list, int)), "Invalid argument to roll!"
+    if isinstance(diceList, int):
+        diceList = [ diceList ]     #  recast a single number as a list
     result = 0
     for dice in diceList:
-        if type(dice) == int:
+        if isinstance(dice, int):
             assert dice > 0, "Negative number supplied as number of sides of die!"
             result += random.choice(range(1, dice+1))
-        elif type(dice) == list:
+        elif isinstance(dice, list):
             result += random.choice(dice)
     return result
 
